fdc8c7180729a50f9050d856bc3929a5525eef25
christianbarkowsky <mail@christianbarkowsky.de>
2012-03-05 15:13:50 +0100

    1.3.1 stable

--- TL_ROOT/system/modules/tabcontrol/ContentTabControl.php
+++ TL_ROOT/system/modules/tabcontrol/ContentTabControl.php
@@ -1,4 +1,7 @@
-<?php if (!defined('TL_ROOT')) die('You can not access this file directly!');
+<?php
+
+if (!defined('TL_ROOT'))
+    die('You can not access this file directly!');
 
 /**
  * TYPOlight webCMS
@@ -19,7 +22,7 @@
  * Software Foundation website at http://www.gnu.org/licenses/.
  *
  * PHP version 5
- * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2011
+ * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2012
  * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Mirco Rahn <http://www.complus-ag.net>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
  * @package    TabControl
  * @license    GNU/LGPL
@@ -29,49 +32,46 @@
 /**
  * Class ContentTabControl
  *
- * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2011
+ * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2012
  * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Mirco Rahn <http://www.complus-ag.net>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
  * @package    Controller
  */
-class ContentTabControl extends ContentElement {
-
+class ContentTabControl extends ContentElement
+{
     /**
-     * Contains the default classes used in our
-     * tab-template
-     *
-     * @staticvar array
-     * @access private
+     * Contains the default classes used in our tab-template
      */
     private static $defaultClasses = array('tabs', 'panes');
 
+
     /**
      * Contains the path to the js-plugin needed for Tabcontrols to work
-     *
-     * @var string
-     * @access private
      */
     private $strPlugin = 'plugins/tabcontrol/tabcontrol.js';
 
+
     /**
      * The template
-     *
-     * @var string
-     * @access protected
      */
     protected $strTemplate = 'ce_tabcontrol_tab';
+
 
     /**
      * Generate content element
      */
-    protected function compile() {
+    protected function compile()
+    {
         //init vars
         $classes = deserialize($this->tabClasses); //come all ye classes ;)
         $titles = deserialize($this->tabTitles); //will only be filled when in tab-mode
-        static $panelIndex = 0;       //static index counter
+        static $panelIndex = 0; //static index counter
+
         //default classes if neccessary
-        if (!count($classes)) {
+        if (!count($classes))
+        {
             $classes = self::$defaultClasses;
-        } else {
+        } else
+        {
             if (!strlen($classes[0]))
                 $classes[0] = self::$defaultClasses[0];
             if (!strlen($classes[1]))
@@ -79,13 +79,16 @@ class ContentTabControl extends ContentElement {
         }
 
         //take some measures depending on the selected tab type
-        switch ($this->tabType) {
+        switch ($this->tabType)
+        {
             case 'tabcontrolstart':
                 //start a new panel
-                if (TL_MODE == 'FE') {
+                if (TL_MODE == 'FE')
+                {
                     $this->strTemplate = 'ce_tabcontrol_start';
                     $this->Template = new FrontendTemplate($this->strTemplate);
-                } else {
+                } else
+                {
                     $this->strTemplate = 'be_wildcard';
                     $this->Template = new BackendTemplate($this->strTemplate);
                     $this->Template->wildcard = '### TabControl: ' . (++$panelIndex) . '. Pane START ###';
@@ -94,10 +97,12 @@ class ContentTabControl extends ContentElement {
 
             case 'tabcontrolstop':
                 //stop the current panel
-                if (TL_MODE == 'FE') {
+                if (TL_MODE == 'FE')
+                {
                     $this->strTemplate = 'ce_tabcontrol_stop';
                     $this->Template = new FrontendTemplate($this->strTemplate);
-                } else {
+                } else
+                {
                     $this->strTemplate = 'be_wildcard';
                     $this->Template = new BackendTemplate($this->strTemplate);
                     $this->Template->wildcard = '### TabControl: ' . $panelIndex . '. Pane END ###';
@@ -107,13 +112,13 @@ class ContentTabControl extends ContentElement {
             case 'tabcontroltab':
             default:
                 //display some tabs and check whether we need to append our plugin
-                if (TL_MODE == 'FE')
-                {
-                    //make sure our plugin will be loaded
-                    if (!is_array($GLOBALS['TL_JAVASCRIPT']))
-                    {
-                    	// Hook
-                    	if (isset($GLOBALS['TL_HOOKS']['tabControlJS']) && is_array($GLOBALS['TL_HOOKS']['tabControlJS']))
+				if (TL_MODE == 'FE')
+				{
+					//make sure our plugin will be loaded
+					if (!is_array($GLOBALS['TL_JAVASCRIPT']))
+					{
+						// Hook
+						if (isset($GLOBALS['TL_HOOKS']['tabControlJS']) && is_array($GLOBALS['TL_HOOKS']['tabControlJS']))
 						{
 							foreach ($GLOBALS['TL_HOOKS']['tabControlJS'] as $callback)
 							{
@@ -125,11 +130,11 @@ class ContentTabControl extends ContentElement {
 						{
 							$GLOBALS['TL_JAVASCRIPT'] = array($this->strPlugin);
 						}
-                    }
-                    elseif (!in_array($this->strPlugin, $GLOBALS['TL_JAVASCRIPT']))
-                    {
-                    	// Hook
-                    	if (isset($GLOBALS['TL_HOOKS']['tabControlJS']) && is_array($GLOBALS['TL_HOOKS']['tabControlJS']))
+					}
+					elseif (!in_array($this->strPlugin, $GLOBALS['TL_JAVASCRIPT']))
+					{
+						// Hook
+						if (isset($GLOBALS['TL_HOOKS']['tabControlJS']) && is_array($GLOBALS['TL_HOOKS']['tabControlJS']))
 						{
 							foreach ($GLOBALS['TL_HOOKS']['tabControlJS'] as $callback)
 							{
@@ -139,15 +144,18 @@ class ContentTabControl extends ContentElement {
 						}
 						else
 						{
-                        	$GLOBALS['TL_JAVASCRIPT'][] = $this->strPlugin;
-                        }
-                    }
+							$GLOBALS['TL_JAVASCRIPT'][] = $this->strPlugin;
+						}
+					}
 
-                    //finally, we set template
-                    $this->Template = new FrontendTemplate($this->strTemplate);
-                } else {
+					//finally, we set template
+					$this->Template = new FrontendTemplate($this->strTemplate);
+                } else
+                {
                     $titleList = '';
-                    foreach ($titles as $index => $title) {
+
+                    foreach ($titles as $index => $title)
+                    {
                         $titleList .=++$index . '. ' . $title . '<br/>';
                     }
 
@@ -158,6 +166,8 @@ class ContentTabControl extends ContentElement {
                 }
         }
 
+        $articleAlias = $this->getArticleAlias($this->pid);
+
         //and pass it all to the template
         $this->Template->behaviour = $this->tabBehaviour;
         $this->Template->panes = $classes[1];
@@ -166,10 +176,26 @@ class ContentTabControl extends ContentElement {
         $this->Template->tabsSelector = '.' . str_replace(' ', '.', $classes[0]);
         $this->Template->titles = $titles;
         $this->Template->id = $this->id;
+        $this->Template->articleAlias = $articleAlias ? "#" . $articleAlias : ".mod_article";
         $this->Template->tab_autoplay_autoSlide = $this->tab_autoplay_autoSlide;
         $this->Template->tab_autoplay_delay = $this->tab_autoplay_delay;
     }
 
+
+	/**
+	 * Get article alias
+	 */
+    protected function getArticleAlias($pid)
+    {
+        $objArticle = $this->Database->prepare("SELECT id, alias FROM tl_article WHERE id=?")->limit(1)->execute($pid);
+
+        if ($objArticle->numRows)
+        {
+            return $objArticle->alias;
+        }
+
+        return null;
+    }
 }
 
 ?>
