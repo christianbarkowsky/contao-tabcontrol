4da42b3c95cdb061990405efcbd9877d789f3333
Christian Barkowsky <mail@christianbarkowsky.de>
2012-08-23 14:32:03 +0200

    Comments

--- system/modules/tabcontrol/ContentTabControl.php
+++ system/modules/tabcontrol/ContentTabControl.php
@@ -1,39 +1,39 @@
-<?php
-
-if (!defined('TL_ROOT'))
-    die('You can not access this file directly!');
+<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
 
 /**
- * TYPOlight webCMS
- * Copyright (C) 2005 Leo Feyer
+ * Contao Open Source CMS
+ * Copyright (C) 2005-2012 Leo Feyer
+ *
+ * Formerly known as TYPOlight Open Source CMS.
  *
  * This program is free software: you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation, either
- * version 2.1 of the License, or (at your option) any later version.
- *
+ * version 3 of the License, or (at your option) any later version.
+ * 
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
- *
+ * 
  * You should have received a copy of the GNU Lesser General Public
  * License along with this program. If not, please visit the Free
- * Software Foundation website at http://www.gnu.org/licenses/.
+ * Software Foundation website at <http://www.gnu.org/licenses/>.
  *
  * PHP version 5
- * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2012
- * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Mirco Rahn <http://www.complus-ag.net>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
+ * @copyright  Christian Barkowsky, Jean-Bernard Valentaten 2009-2012
+ * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
  * @package    TabControl
  * @license    GNU/LGPL
  * @filesource
  */
 
+
 /**
  * Class ContentTabControl
  *
- * @copyright  Christian Barkowsky, Mirco Rahn, Jean-Bernard Valentaten 2009-2012
- * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Mirco Rahn <http://www.complus-ag.net>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
+ * @copyright  Christian Barkowsky, Jean-Bernard Valentaten 2009-2012
+ * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
  * @package    Controller
  */
 class ContentTabControl extends ContentElement
@@ -179,6 +179,8 @@ class ContentTabControl extends ContentElement
         $this->Template->articleAlias = $articleAlias ? "#" . $articleAlias : ".mod_article";
         $this->Template->tab_autoplay_autoSlide = $this->tab_autoplay_autoSlide;
         $this->Template->tab_autoplay_delay = $this->tab_autoplay_delay;
+        
+        $this->Template->class = 'klasse';
     }
 
 
