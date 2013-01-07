6c884b042a998392d0af7fa9d0599b98a0d49399
Christian Barkowsky <mail@christianbarkowsky.de>
2013-01-07 16:28:35 +0100

    first import for the contao 3.0 version

--- elements/ContentTabControl.php
+++ elements/ContentTabControl.php
@@ -1,43 +1,21 @@
-<?php if (!defined('TL_ROOT')) die('You cannot access this file directly!');
+<?php
 
 /**
- * Contao Open Source CMS
- * Copyright (C) 2005-2012 Leo Feyer
- *
- * Formerly known as TYPOlight Open Source CMS.
- *
- * This program is free software: you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation, either
- * version 3 of the License, or (at your option) any later version.
+ * TabControl
  * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
- * Lesser General Public License for more details.
- * 
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, please visit the Free
- * Software Foundation website at <http://www.gnu.org/licenses/>.
- *
- * PHP version 5
- * @copyright  Christian Barkowsky, Jean-Bernard Valentaten 2009-2012
+ * @copyright  Christian Barkowsky 2012-2013, Jean-Bernard Valentaten 2009-2012
+ * @package    tabControl
  * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
- * @package    TabControl
- * @license    GNU/LGPL
- * @filesource
+ * @license    LGPL
  */
+ 
+ 
+namespace Contao;
 
 
-/**
- * Class ContentTabControl
- *
- * @copyright  Christian Barkowsky, Jean-Bernard Valentaten 2009-2012
- * @author     Christian Barkowsky <http://www.christianbarkowsky.de>, Jean-Bernard Valentaten <troggy.brains@gmx.de>
- * @package    Controller
- */
-class ContentTabControl extends ContentElement
+class ContentTabControl extends \ContentElement
 {
+
 	/**
      * Template
      */
