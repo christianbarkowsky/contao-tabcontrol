7c33091115ecde2936bf6b9ef7b7b9ebf64f9f59
Christian Barkowsky <mail@christianbarkowsky.de>
2013-03-21 16:54:41 +0100

    Runonce

--- config/runonce.php
+++ config/runonce.php
@@ -3,13 +3,20 @@
 /**
  * TabControl
  * 
- * @copyright  Christian Barkowsky 2012-2013
+ * @copyright  Christian Barkowsky 2013
  * @package    tabControl
  * @author     Christian Barkowsky <http://www.christianbarkowsky.de>
  * @license    LGPL
  */
+ 
 
-class TabControlRunonce extends Controller
+/**
+ * Run in a custom namespace, so the class can be replaced
+ */
+namespace Contao;
+
+
+class TabControlRunonce extends \Controller
 {
 
 	/**
@@ -18,7 +25,6 @@ class TabControlRunonce extends Controller
 	public function __construct()
 	{
 		parent::__construct();
-
 		$this->import('Database');
 	}
 
@@ -28,6 +34,31 @@ class TabControlRunonce extends Controller
 	 */
 	public function run()
 	{
+		$objTabControl = $this->Database->query("SELECT * FROM tl_content WHERE type='tabcontrol'");
+
+		while($objTabControl->next())
+		{
+		
+			if($objTabControl->tabType == 'tabcontroltab')
+			{
+				//$this->Database->prepare("UPDATE tl_content SET tab_template=? WHERE id=?")->execute('ce_tabcontrol_tab', $objStores->id);
+			}
+			
+			if($objTabControl->tabType == 'tabcontrolstart')
+			{
+				//$this->Database->prepare("UPDATE tl_content SET tab_template_start=? WHERE id=?")->execute('ce_tabcontrol_start', $objStores->id);
+			}
+			
+			if($objTabControl->tabType == 'tabcontrolstop')
+			{
+				//$this->Database->prepare("UPDATE tl_content SET tab_template_stop=? WHERE id=?")->execute('ce_tabcontrol_stop', $objStores->id);
+			}
+			
+			if($objTabControl->tabType == 'tabcontrol_end')
+			{
+				//$this->Database->prepare("UPDATE tl_content SET tab_template_end=? WHERE id=?")->execute('ce_tabcontrol_end', $objStores->id);
+			}		
+		}
 	}
 }
 
